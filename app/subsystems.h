e015b2c6431404d80f4bbddf962077cff67c4ee8
jhlmarques <josehenriquelm@gmail.com>
2023-07-04 11:13:40 -0300

    WOL Subsystem classes

--- app/subsystems.h
+++ app/subsystems.h
@@ -4,3 +4,37 @@ void discoverySubsystemThread();
 void monitoringSubsystemThread();
 void managementSubsystemThread();
 void interfaceSubsystemThread();
+
+// Subsistema genérico do WakeOnLan
+class WOLSubsystem{
+    private:
+    bool isManager;
+
+    public:
+    WOLSubsystem(bool _isManager) : isManager(_isManager) {};
+    
+    virtual bool start() = 0; // Inicializa o subsistema
+    virtual void stop() = 0; // Encerra o subsistema
+
+
+};
+
+// Subsistema de descoberta
+class DiscoverySS : public WOLSubsystem{
+
+};
+
+// Subsistema de monitoramento
+class MonitoringSS : public WOLSubsystem{
+
+};
+
+// Subsistema de gerenciamento
+class ManagementSS : public WOLSubsystem{
+
+};
+
+// Subsistema de interface
+class InterfaceSS : public WOLSubsystem{
+
+};
